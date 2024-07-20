ac02c8f456099439aae1d065e5c0a45e86eb2e36
AaryaHA <haprasad2000@gmail.com>
2024-07-20 21:04:49 +0530

    Update colour_code.h

--- colour_code.h
+++ colour_code.h
@@ -4,23 +4,20 @@
 enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
 enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 
-typedef struct 
-{
+extern const char* MajorColorNames[];
+extern const char* MinorColorNames[];
+extern const int numberOfMajorColors;
+extern const int numberOfMinorColors;
+extern const int MAX_COLORPAIR_NAME_CHARS;
+
+typedef struct {
     enum MajorColor majorColor;
     enum MinorColor minorColor;
 } ColorPair;
 
-extern const char* MajorColorNames[];
-extern int numberOfMajorColors;
-extern const char* MinorColorNames[];
-extern const int MAX_COLORPAIR_NAME_CHARS;
-extern int numberOfMinorColors;
+void ColorPairToString(const ColorPair* colorPair, char* buffer);
+ColorPair GetColorFromPairNumber(int pairNumber);
+int GetPairNumberFromColor(const ColorPair* colorPair);
+void PrintColorCodingManual();
 
-extern int GetPairNumberFromColor(const ColorPair* colorPair);
-extern ColorPair GetColorFromPairNumber(int pairNumber);
-extern void ColorPairToString(const ColorPair* colorPair, char* buffer) ;
-extern void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
-extern void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber);
-
-
-#endif /* COLOR_CODE_H */
+#endif
