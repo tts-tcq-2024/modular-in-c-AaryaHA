3f0c68a1b1d814bf41000d85daca20320ddc56ca
AaryaHA <haprasad2000@gmail.com>
2024-07-20 20:37:28 +0530

    Update colour_code.c

--- colour_code.c
+++ colour_code.c
@@ -2,26 +2,37 @@
 #include <string.h>
 #include "color_code.h"
 
-
-const char* MajorColorNames[] = {
+const char* MajorColorNames[] = 
+{
     "White", "Red", "Black", "Yellow", "Violet"
 };
-int numberOfMajorColors =
-    sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
-const char* MinorColorNames[] = {
+
+int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+
+const char* MinorColorNames[] = 
+{
     "Blue", "Orange", "Green", "Brown", "Slate"
 };
+
 const int MAX_COLORPAIR_NAME_CHARS = 16;
-int numberOfMinorColors =
-    sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
-	
-void ColorPairToString(const ColorPair* colorPair, char* buffer) {
+
+int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+
+int GetPairNumberFromColor(const ColorPair* colorPair) 
+{
+    return colorPair->majorColor * numberOfMinorColors +
+            colorPair->minorColor + 1;
+}
+
+void ColorPairToString(const ColorPair* colorPair, char* buffer) 
+{
     sprintf(buffer, "%s %s",
         MajorColorNames[colorPair->majorColor],
         MinorColorNames[colorPair->minorColor]);
 }
-	
-ColorPair GetColorFromPairNumber(int pairNumber) {
+
+ColorPair GetColorFromPairNumber(int pairNumber) 
+{
     ColorPair colorPair;
     int zeroBasedPairNumber = pairNumber - 1;
     colorPair.majorColor = 
@@ -30,13 +41,6 @@ ColorPair GetColorFromPairNumber(int pairNumber) {
         (enum MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
     return colorPair;
 }
-
-int GetPairNumberFromColor(const ColorPair* colorPair) {
-    return colorPair->majorColor * numberOfMinorColors +
-            colorPair->minorColor + 1;
-}
-
-
 void testNumberToPair(int pairNumber,
     enum MajorColor expectedMajor,
     enum MinorColor expectedMinor)
@@ -61,4 +65,3 @@ void testPairToNumber(
     printf("Got pair number %d\n", pairNumber);
     assert(pairNumber == expectedPairNumber);
 }
-
