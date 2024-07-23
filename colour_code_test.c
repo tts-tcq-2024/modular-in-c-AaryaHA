ee05bfd539bb239bcd6178496098c8b38503c08e
AaryaHA <haprasad2000@gmail.com>
2024-07-23 16:42:26 +0530

    Update colour_code_test.c

--- colour_code_test.c
+++ colour_code_test.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <assert.h>
-#include "color_code.h"
+#include "colour_code.h"
 
 void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor) {
     ColorPair colorPair = GetColorFromPairNumber(pairNumber);
